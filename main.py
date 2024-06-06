a8bea653dfefe4d4183826ddb435b3e49d1a3eaf
Kyle Scritchfield <kescritch@gmail.com>
2024-06-06 13:11:06 +0200

    update

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from uiRunner import Ui_MainWindow
 from PyQt6 import QtWidgets
+import sys
 
-if __name__ == "__main__":
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec())
 
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec())
-    
     
 
 
